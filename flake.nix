8084903b7b4839cdb0e809d1206f9f56276f9398
Empa <emanuel@empa.xyz>
2022-09-05 19:22:05 +0200

    Add flake and ignore direnv

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "nixpkgs";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.bashInteractive ];
+          buildInputs = with pkgs; [ stack ormolu hlint ];
+        };
+      });
+}
